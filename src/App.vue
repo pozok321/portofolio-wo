6c1230c620ab6d743c22b29c99f0ec4b91c21050
valeryan <valeryanr18@gmail.com>
2022-11-15 14:42:23 +0700

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Navbar/>
   <Content/>
+  <Footer/>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
     Navbar,
     Content,
+    Footer,
   }
 }
 </script>
